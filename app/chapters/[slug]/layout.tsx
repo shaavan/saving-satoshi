645ac848d8677108bc7ea070c0a9b6f5446917a7
shaavan <shaavan.github@gmail.com>
2023-02-08 21:17:43 +0530

    exp: Added a working version of automatically changing rendered component based on screen size
    - TODO:
    	1. There is a warning in browser console but seems unrelated to
    	   PR.
    	2. Seperate the added code in its own function so that it can be
    	   reused.
    	3. Initially the isDesktopSize is always rendered as false.
    	   Change it so that it can predict the window size
    unconditionally

--- app/chapters/[slug]/layout.tsx
+++ app/chapters/[slug]/layout.tsx
@@ -1,9 +1,25 @@
+'use client'
+
 import Navbar, { NavbarMobile } from 'components/Navbar'
 import chapters from 'content/chapters'
+import { useEffect, useState } from 'react'
 
-export default async function Layout({ children, params }) {
+//TODO:
+// This function is async in nature. But you can't use functional components with async function.
+// For now remove the async but later introduce them back and seperate functional component part.
+export default function Layout({ children, params }) {
   const isValidChapter = chapters[params.slug]
-  const isDesktopSize = false
+  // const isDesktopSize = false
+
+  const [isDesktopSize, setIsDesktopSize] = useState(false)
+  const handleResize = () => setIsDesktopSize(window.innerWidth > 768)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  })
+
+  console.log(isDesktopSize)
 
   return (
     <div className="ch1-background flex flex-col">
