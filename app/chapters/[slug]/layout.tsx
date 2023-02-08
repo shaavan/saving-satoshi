a1eec0828a1b89ebdccfe5ddba426db2b7a45b7a
shaavan <shaavan.github@gmail.com>
2023-02-08 20:53:09 +0530

    exp: Added a prototype for NavbarMobile and render it based on a static bool variable in layout.tsx

--- app/chapters/[slug]/layout.tsx
+++ app/chapters/[slug]/layout.tsx
@@ -1,13 +1,18 @@
-import Navbar from 'components/Navbar'
+import Navbar, { NavbarMobile } from 'components/Navbar'
 import chapters from 'content/chapters'
 
 export default async function Layout({ children, params }) {
   const isValidChapter = chapters[params.slug]
+  const isDesktopSize = false
 
   return (
     <div className="ch1-background flex flex-col">
       <div className="fix-grow-issue flex min-h-screen flex-col blur">
-        {isValidChapter && <Navbar params={params} />}
+        {isValidChapter && isDesktopSize ? (
+          <Navbar params={params} />
+        ) : (
+          <NavbarMobile />
+        )}
         {children}
       </div>
     </div>
