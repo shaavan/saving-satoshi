aaf7152ae71277ea33e029d737887833297b4004
shaavan <shaavan.github@gmail.com>
2023-02-12 23:26:41 +0530

    Modifies the layout.tsx to display the NavbarMobile when the window size becomes smaller than desktop size

--- app/chapters/[slug]/layout.tsx
+++ app/chapters/[slug]/layout.tsx
@@ -1,13 +1,37 @@
-import Navbar from 'components/Navbar'
-import chapters from 'content/chapters'
+'use client'
 
-export default async function Layout({ children, params }) {
+import Navbar, { NavbarMobile } from 'components/Navbar'
+import chapters from 'content/chapters'
+import { useEffect, useState } from 'react'
+
+function getWindowWidth() {
+  const { innerWidth: width } = window
+  return width
+}
+
+export default function Layout({ children, params }) {
   const isValidChapter = chapters[params.slug]
+
+  const desktopSize = 800
+
+  const windowWidth = getWindowWidth()
+  const [isDesktopSize, setIsDesktopSize] = useState(windowWidth > desktopSize)
+
+  const handleResize = () => setIsDesktopSize(window.innerWidth > desktopSize)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  })
 
   return (
     <div className="ch1-background flex flex-col">
       <div className="fix-grow-issue flex min-h-screen flex-col blur">
-        {isValidChapter && <Navbar params={params} />}
+        {isValidChapter && isDesktopSize ? (
+          <Navbar params={params} />
+        ) : (
+          <NavbarMobile params={params} />
+        )}
         {children}
       </div>
     </div>
