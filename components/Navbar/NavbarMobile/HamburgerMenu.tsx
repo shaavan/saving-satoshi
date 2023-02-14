837882615fa2bd74a852d44970d5709b6d774195
shaavan <shaavan.github@gmail.com>
2023-02-14 19:52:23 +0530

    Replaced styles with class in Hamburgermenu.tsx file

--- components/Navbar/NavbarMobile/HamburgerMenu.tsx
+++ components/Navbar/NavbarMobile/HamburgerMenu.tsx
@@ -1,37 +1,29 @@
+import clsx from 'clsx'
 import React, { useState } from 'react'
 
 export default function HamburgerMenu(props) {
-  const styles = {
-    line: {
-      height: '2px',
-      width: '20px',
-      background: 'white',
-      transition: 'all 0.2s ease',
-    },
-    lineTop: {
-      transform: props.isOpen
-        ? ' translateX(0) translateY(-1px) rotate(-45deg)'
-        : 'none',
-      transformOrigin: 'top right',
-      marginBottom: '5px',
-    },
-    lineBottom: {
-      transform: props.isOpen
-        ? ' translateX(1px) translateY(4px) rotate(45deg)'
-        : 'none',
-      transformOrigin: 'top right',
-      marginTop: '2px',
-      width: props.isOpen ? '20px' : '15px',
-    },
-  }
+  const className =
+    'h-0.5 w-5 bg-white transition-all ease-in-out origin-top-right'
 
   return (
     <div
       className="mr flex h-8 w-10 cursor-pointer flex-col items-start justify-center overflow-hidden border-r border-white/25 p-1"
       onClick={props.clicked}
     >
-      <div style={{ ...styles.line, ...styles.lineTop }} />
-      <div style={{ ...styles.line, ...styles.lineBottom }} />
+      <div
+        className={clsx(className, 'mb-[5px]', {
+          'transform-none': !props.isOpen,
+          'translate-x-0 -translate-y-[1.8px] -rotate-45': props.isOpen,
+        })}
+      />
+
+      <div
+        className={clsx(className, 'mt-[2px]', {
+          'w-[15px] transform-none': !props.isOpen,
+          'w-[20px] translate-x-[1.2px] translate-y-[3px] rotate-45':
+            props.isOpen,
+        })}
+      />
     </div>
   )
 }
