3448d68743173cb1b22a6409e122ef535bf429f0
shaavan <shaavan.github@gmail.com>
2023-02-15 18:43:01 +0530

    exp: Replicated current animation with pure tailwind

--- components/Navbar/NavbarMobile/TabGroupMobile.tsx
+++ components/Navbar/NavbarMobile/TabGroupMobile.tsx
@@ -24,15 +24,6 @@ export default function TabGroup({ isOpen, clicked, params }) {
 
   return (
     <div className="mx-4 flex h-full flex-col items-stretch">
-      {/* <Transition
-        show={isOpen}
-        enter="transition-opacity ease-linear duration-600 delay-400"
-        enterFrom="opacity-0"
-        enterTo="opacity-100 "
-        leave="transition-opacity ease-linear duration-800"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      > */}
       <div
         className={clsx(
           'px-2 py-3 text-2xl text-white/50',
@@ -47,26 +38,23 @@ export default function TabGroup({ isOpen, clicked, params }) {
         <span className="ml-1 text-white">{chapterMeta.title}</span>
       </div>
 
-      {/* <Transition.Child
-          enter="transition ease-in-out duration-600 transform"
-          enterFrom="-translate-x-full"
-          enterTo="translate-x-0"
-          leave="transition ease-in-out duration-800 transform"
-          leaveFrom="translate-x-0"
-          leaveTo="-translate-x-full"
-        > */}
       {challenges.map((challenge, index) => (
-        <TabMobile
-          key={index}
-          count={challenges.length}
-          index={index}
-          challenge={challenge}
-          params={params}
-          clicked={clicked}
-        />
+        <div
+          className={clsx('duration-600 transform transition ease-in-out', {
+            '-translate-x-[110%]': !isOpen,
+            'translate-x-0': isOpen,
+          })}
+        >
+          <TabMobile
+            key={index}
+            count={challenges.length}
+            index={index}
+            challenge={challenge}
+            params={params}
+            clicked={clicked}
+          />
+        </div>
       ))}
-      {/* </Transition.Child>
-      </Transition> */}
     </div>
   )
 }
