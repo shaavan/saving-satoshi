7066f0eb9e6df8610d2034c3f759fb7e8c411d82
shaavan <shaavan.github@gmail.com>
2023-02-15 20:20:07 +0530

    Add the Menu.tsx file responsible for display dropdown menu's background
    - Also introduce a new globals.css class and use it to turn Menu's
      background opaque

---
+++ components/Navbar/NavbarMobile/Menu.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import TabGroupMobile from 'components/Navbar/NavbarMobile/TabGroupMobile'
+import clsx from 'clsx'
+
+export default function Menu(props) {
+  return (
+    <div
+      id="lesson-navigation-menu"
+      className={clsx(
+        'blur-opaque absolute z-10 flex w-full flex-col transition-[height] duration-[400ms] ease-out',
+        {
+          'delay-200': !props.isOpen,
+        }
+      )}
+      style={{
+        height: `${props.isOpen ? `calc(100% - ${props.position}px)` : `0px`}`,
+        top: `${props.position}px`,
+      }}
+      aria-modal="true"
+      aria-hidden={!props.isOpen}
+      aria-label="Lesson Navigation Menu"
+    >
+      {
+        <TabGroupMobile
+          isOpen={props.isOpen}
+          clicked={props.clicked}
+          params={props.params}
+        />
+      }
+    </div>
+  )
+}
