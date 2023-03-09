53633ea6fcc8073669cbf9ebe4a77b195eebd5a7
shaavan <shaavan.github@gmail.com>
2023-03-09 21:28:46 +0530

    Introduce the new Outro Page

---
+++ content/lessons/outro/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { useTranslations } from 'hooks'
+import { TextImage } from 'ui'
+
+export default function Outro({ lang }) {
+  const t = useTranslations(lang)
+  return (
+    <TextImage
+      lang={lang}
+      imageSrc="/assets/images/copter-interior-upscaled.jpg"
+      imageAlt={t('outro.title')}
+      btnText={t('outro.button_text')}
+      btnEnabled={true}
+      next={'/chapters/chapter-1/done'}
+    >
+      <p>{t('outro.paragraph_one')}</p>
+      <p className="pt-6">{t('outro.paragraph_two')}</p>
+    </TextImage>
+  )
+}
+
+export const metadata = {
+  title: 'outro.title',
+}
