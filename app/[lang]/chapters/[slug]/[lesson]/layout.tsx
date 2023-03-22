ec07a2f2a22e8d402fea6eaf1f792e54418b1b25
shaavan <shaavan.github@gmail.com>
2023-03-22 17:08:03 +0530

    Add per-page theming option

--- app/[lang]/chapters/[slug]/[lesson]/layout.tsx
+++ app/[lang]/chapters/[slug]/[lesson]/layout.tsx
@@ -1,3 +1,18 @@
-export default function Layout({ children }) {
-  return <div className="flex grow flex-col">{children}</div>
+import Navbar from 'components/Navbar'
+import { lessons } from 'content'
+
+export default function Layout({ children, params }) {
+  const { slug, lesson: lessonId } = params
+
+  const defaultTheme = 'bg-back'
+  const { theme = defaultTheme } = lessons[slug][lessonId].metadata
+
+  return (
+    <div className={`${theme} flex flex-col`}>
+      <div className="fix-grow-issue flex min-h-screen flex-col overflow-hidden bg-back/75 drop-shadow-3xl backdrop-blur-4xl">
+        <Navbar params={params} />
+        {children}
+      </div>
+    </div>
+  )
 }
