19cfcbc9ffaf9690718cc4bd0f08dd685cd50ec1
shaavan <shaavan.github@gmail.com>
2023-03-23 21:39:42 +0530

    Remove redundant 'use client' from lesson content.
    - This piece of code interferes with proper reading of the file's
      metadata.

--- content/lessons/chapter-1/genesis-2.tsx
+++ content/lessons/chapter-1/genesis-2.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useTranslations } from 'hooks'
 import { Button, LessonInfo, Title, Text, InputChallenge } from 'ui'
 
--- content/lessons/chapter-1/transacting-2.tsx
+++ content/lessons/chapter-1/transacting-2.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useTranslations } from 'hooks'
 import { LessonInfo, Text, Title } from 'ui'
 import { InputChallenge } from 'ui'
