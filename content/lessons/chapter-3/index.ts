70f79e9a115807b777557d1171dae4f55eca24ae
shaavan <shaavan.github@gmail.com>
2023-06-12 19:36:27 +0530

    Introduce Chapter-3 Intros
    - This PR introduces the Intro page and introduces the subsequent
      updates to progress to keep track of them.
    - This PR also temporarily introduces challenge-1 for Chapter-3, which
      is introduced to check correct functioning of "Continue" button. It
    will be removed before the final merge of PR.

---
+++ content/lessons/chapter-3/index.ts
@@ -0,0 +1,11 @@
+import * as Intro1 from './intro-1'
+import * as Intro2 from './intro-2'
+import * as YVB1 from './you-vs-bit-1'
+
+const Chapter3 = {
+  'intro-1': Intro1,
+  'intro-2': Intro2,
+  'you-vs-bit-1': YVB1,
+}
+
+export default Chapter3
